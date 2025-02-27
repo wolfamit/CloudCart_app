534c55ef37a22a52e70c3fe62080acb2f6f3cf32
Amitabh <sonuambastha117@gmail.com>
2025-02-27 15:15:11 +0530

    fv

--- ws-server-2/index.js
+++ ws-server-2/index.js
@@ -1,6 +1,7 @@
-const WebSocket = require('ws');
+const WebSocket = require('ws')
 
-const server = new WebSocket.Server({ port: 8080 });
+const server = new WebSocket.Server({ port: 8080 })
+
 
 server.on('connection', (ws) => {
     console.log('New client connected');
