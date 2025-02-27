e011358d035baa371fb654b15ab688d5ab24c74e
Amitabh <sonuambastha117@gmail.com>
2025-02-27 14:59:26 +0530

    first commit

---
+++ cloudcart-react/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Hii there
+    </div>
+  );
+}
+
+export default App;
---
+++ ws-server-2/README.md
@@ -0,0 +1,3 @@
+User can sign in using cart id;
+Endpoint for adding items in the db done;
+add item to the user cart to be done by ws;
